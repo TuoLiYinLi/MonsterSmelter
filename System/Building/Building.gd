980dda9b1acb6aa0c4a09152b3f17d19c5ae10f9
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:36:37 +0800

    新的接口
    * 当被子弹击中时触发on_hit_by

--- System/Building/Building.gd
+++ System/Building/Building.gd
@@ -4,17 +4,22 @@
 extends Node2D
 class_name Building
 
+# 所在的网格
 var grid = null setget set_grid
 
 func set_grid(g):
 	grid = g
 	position = grid.position
 
-#检测函数Z
-func is_Building():
-	pass
-
 # 类似版本号
 var version:int = 0 setget set_version
 func set_version(value:int):
 	$Sprite.frame = value
+
+# 当被子弹击中时
+func on_hit_by(_projectile):
+	pass
+
+#------------------------------------------------------
+static func is_Building(obj)->bool:
+	return obj.has_method("is_Building")
