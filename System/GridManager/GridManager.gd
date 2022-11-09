999322af005babda655417133c45b5833ae23390
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:40:06 +0800

    新增十字查找函数
    * check_in_cross 检测grid_target是否在grid_center的直线方向上、它在哪个方向以及距离

--- System/GridManager/GridManager.gd
+++ System/GridManager/GridManager.gd
@@ -98,37 +98,30 @@ func to_navigate_matrix(instance:Object, method_name:String)->Array:
 
 
 # 计算两个网格之间的距离
-func distance(grid1, grid2)->int:
+func distance(grid1:Grid, grid2:Grid)->int:
 	return int(abs(grid1.index_x - grid2.index_x) + abs(grid1.index_y - grid2.index_y))
 	
-
-#up 1 down 2 left 3 right 4
-
-#判断范围内有没有敌人Z
-func finder_enemy(distance:int,grid)->int:
-	#导出普通地图
-	var mt = to_navigate_matrix(G,"simple_navigate_matrix")
+# 检测grid_target是否在grid_center的直线方向上、它在哪个方向以及距离
+func check_in_cross(grid_center:Grid, grid_target:Grid)->Array:
+	# 是否在十字方向上
+	var in_cross:bool = false
+	# 方向
+	var direction:int
+	# 距离
+	var d:int
+	if(grid_center.index_x == grid_target.index_x):
+		d = abs(grid_center.index_y - grid_target.index_y)
+		in_cross = true
+		if(grid_target.index_y < grid_center.index_y):
+			direction = G.DIRECTION.UP
+		else:
+			direction = G.DIRECTION.DOWN
+	elif(grid_center.index_y == grid_target.index_y):
+		in_cross =true
+		d = abs(grid_center.index_x - grid_target.index_x)
+		if(grid_target.index_x < grid_center.index_x):
+			direction = G.DIRECTION.LEFT
+		else:
+			direction = G.DIRECTION.RIGHT
 	
-	
-	#检测x轴上是否有怪物
-	for i in range(grid.index_x-distance,grid.index_x+distance+1):
-		if i<0 or i>=len(mt[0]) or i == grid.index_x:
-			continue
-		if mt[i][grid.index_y] == 2:
-			if i > grid.index_x:
-				return Projectile.DIRECTION.RIGHT
-			else:
-				return Projectile.DIRECTION.LEFT
-	
-	#检测y轴上是否有怪物
-	for i in range(grid.index_y-distance,grid.index_y+distance+1):
-		if i<0 or i>=len(mt[0]) or i == grid.index_y:
-			continue
-		if mt[grid.index_x][i] == 2:
-			if i > grid.index_y:
-				return Projectile.DIRECTION.DOWN
-			else:
-				return Projectile.DIRECTION.UP
-	
-	return -1
-
+	return [in_cross, direction, d]
