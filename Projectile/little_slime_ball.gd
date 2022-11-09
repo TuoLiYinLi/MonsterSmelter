4fc4e280a67dfc4b03235fde1d3eb3ed583d217e
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:43:32 +0800

    新的继承投射物：喷吐（粘液球）
    当击中敌人或墙壁时消失，可以存在1s，飞行3.5格

---
+++ Projectile/little_slime_ball.gd
@@ -0,0 +1,24 @@
+# little_slime_ball.gd
+
+extends Projectile
+
+func _ready():
+	# 持续时间
+#	life_time = 10
+	# 初速度
+#	velocity = Vector2.ZERO
+	# 加速度
+#	acceleration = Vector2.ZERO
+	# 速度衰减
+#	damp = 1
+	pass
+
+# 当击中战斗实体时触发
+func on_hit_battle_entity(_battle_entity):
+	if(host and _battle_entity != host):
+		host.offence(_battle_entity)
+		queue_free()
+
+# 当击中建筑时触发
+func on_hit_building(_building):
+	queue_free()
