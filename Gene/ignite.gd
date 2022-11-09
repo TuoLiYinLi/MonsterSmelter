1dee81473cc255364eae45b523cfae7cc67726f4
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:32:25 +0800

    移动文件位置

--- Gene/ignite.gd
+++ Gene/ignite.gd
@@ -1,12 +1,10 @@
-extends Gene
-class_name GeneIgnite
+# ignite.gd
 
+extends Gene
 
 func _ready():
 	# 攻速
 	attack_speed = 0.1
-	#生命恢复
-	health_recovery = 0.2
 
 # 当击中敌人时 enemy是击中的敌人
 func on_attack(enemy):
