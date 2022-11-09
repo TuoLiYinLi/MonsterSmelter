d4ebd804abd2b2cbc455b8c12d80ffc156aecbb5
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:31:50 +0800

    增加颜色设置属性
    color 属性调用 set_color 修改Label的modulate

--- Effect/damage_num.gd
+++ Effect/damage_num.gd
@@ -4,8 +4,12 @@ var text setget set_text
 func set_text(value):
 	$Label.text = str(value)
 
+var color setget set_color
+func set_color(value:Color):
+	$Label.modulate = value
+
 func _process(delta):
 	modulate.a -= delta
 	position.y -= delta * 32
-	if(modulate.a<=0):
+	if(modulate.a <= 0):
 		queue_free()
