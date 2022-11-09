ee9345c8b939810eb89f2f8753813e077658e9ab
脱离引力 <78811591+TuoLiYinLi@users.noreply.github.com>
2022-11-10 00:35:51 +0800

    全新设计
    * 两个继承虚函数on_hit_battle_entity, on_hit_building 分别在击中角色和建筑后触发
    * 初速度、加速度、damp控制物体运动

--- System/Projectile/Projectile.gd
+++ System/Projectile/Projectile.gd
@@ -7,82 +7,53 @@ class_name Projectile
 
 # 自己的主人
 var host:BattleEntity = null
-# 方向
-var direction:int
 
-#位置
-var velocity:Vector2 
+# 初始速度
+var velocity:Vector2 = Vector2.ZERO
 
-var damp
+# 加速度
+var acceleration:Vector2 = Vector2.ZERO
+
+# 速度衰减
+var damp:float = 1
 
 #经过时间
 var life_time:float = 10
 
-# 是否击中后立即销毁
-var destory_on_collision:bool = true
-
-# 是否可以穿墙
-var through:int
-
-enum DIRECTION{
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
-}
-
-# 
-func set_host(hs:BattleEntity,direction:int):
-	self.host = hs
-	self.direction = direction
-	#提前设置好Z
-	self.through = 0
-	self.destory_on_collision = 0
-	#设置旋转角度Z
-	match self.direction:
-		DIRECTION.UP:
-			self.rotation_degrees = -90.0
-			velocity.y = -32
-		DIRECTION.DOWN:
-			self.rotation_degrees = 90.0
-			velocity.y =32
-		DIRECTION.LEFT:
-			self.rotation_degrees = 180.0
-			velocity.x =-32
-		DIRECTION.RIGHT:
-			self.rotation_degrees = 0.0
-			velocity.x =32
-
-#up 1 down 2 left 3 right 4
-func _physics_process(delta):
+func _physics_process(delta:float):
+	# 存在时间
+	life_time -= delta
 	if life_time <= 0:
 		queue_free()
 		return
-	life_time -= delta
-	
-	position += velocity *delta
-	
+	velocity *= damp
+	velocity += acceleration * delta
+	position += velocity * delta
 	
 # 信号接入
 # 当击中战斗实体(BattleEntity)或建筑(Building)时会触发
 func on_enter_area(area:Area2D):
-	#获取父节点
+	# 获取目标的父节点
 	var _target = area.get_parent()
-#	print(_battle_entity)
-	if _target == host:
-		print("[Projectile] %s 击中自己" %self)
+	if(!_target):
+		# 不是战斗实体
 		return
-	#判断能不能穿墙
-	elif _target.has_method("is_Building"):
-		if through:
-			pass
-		else:
-			queue_free()
-			return
-	print("[Projectile] %s 击中敌人 %s"%[self,_target])
-	host.offence(_target)
-	#判断撞击后是否被销毁
-	if destory_on_collision:
-		queue_free()
-	else:
-		pass
+	elif G.is_BattleEntity(_target):
+		# 击中了战斗实体
+		on_hit_battle_entity(_target)
+	elif G.is_Building(_target):
+		# 击中了建筑
+		on_hit_building(_target)
+		_target.on_hit_by(self)
+
+# 当击中战斗实体时触发
+func on_hit_battle_entity(_battle_entity):
+	pass
+
+# 当击中建筑时触发
+func on_hit_building(_building):
+	pass
+
+#------------------------------------------------------
+static func is_Projectile(obj)->bool:
+	return obj.has_method("is_Projectile")
